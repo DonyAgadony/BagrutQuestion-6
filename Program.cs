d875678d9d01fab98f76aac5cbdc6dbd4d058665
DonyAgadony <162630322+DonyAgadony@users.noreply.github.com>
2024-11-22 10:33:30 +0200

    Finished Question

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    public static int Total(int[] arr, int numSeconds)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < numSeconds)
+            {
+                sum += numSeconds / arr[i];
+            }
+        }
+        return sum;
+    }
+
+    public static int MinTime(int[] arr, int amount)
+    {
+        int count = 0;
+        for (int i = 1; i < int.MaxValue; i++)
+        {
+            count = Total(arr, i);
+            if (count >= amount)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public static void Main()
+    {
+        int[] arr = [1, 9, 3];
+        Console.WriteLine(MinTime(arr, 7));
+    }
+}
